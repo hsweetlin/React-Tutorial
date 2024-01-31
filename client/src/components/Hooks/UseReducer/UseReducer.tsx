6176eb1768c95c8ba8fbc719c2d7b4981c2b83a8
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:33:32 +0530

    feat: implement UseReducer component

---
+++ client/src/components/Hooks/UseReducer/UseReducer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './UseReducer.scss';
+
+
+export function UseReducer() {
+  return (
+    <div className="usereducer-container">
+      <div id='middle_container'>
+        <h1>useReducer</h1>
+      </div>
+    </div>
+  )
+}
