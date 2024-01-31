1b303913db8fb249573a5043182ed4f65c13df55
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:35:45 +0530

    feat: implement Styling component

---
+++ client/src/components/Styling/Styling.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Styling.scss';
+
+
+export function Styling() {
+  return (
+    <div className="styling-container">
+      <div id='middle_container'>
+        <h1>Styling</h1>
+      </div>
+    </div>
+  )
+}
