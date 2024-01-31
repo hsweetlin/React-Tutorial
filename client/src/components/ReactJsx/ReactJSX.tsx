04b57ee7ffb28b3d79f87fd3a33b0594a4a8dd81
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 15:38:11 +0530

    feat: implement react jsx component

---
+++ client/src/components/ReactJsx/ReactJSX.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './ReactJSX.scss';
+
+
+export function ReactJSX() {
+  return (
+    <div className="jsx-container">
+     
+      <div id='middle_container'>
+        <h1>React JSX</h1>
+      </div>
+    </div>
+  )
+}
