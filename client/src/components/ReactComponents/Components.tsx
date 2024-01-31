762ab3af1a3c1156c98d7e0c897fb70005d02762
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 15:37:44 +0530

    feat: implement react-components

---
+++ client/src/components/ReactComponents/Components.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Components.scss';
+
+
+export function Components() {
+  return (
+    <div className="component-container">
+      <div id='middle_container'>
+        <h1>Components</h1>
+      </div>
+    </div>
+  )
+}
