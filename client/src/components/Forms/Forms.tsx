b48d2fdd8a82c2adf77eb5848dd3caf76b524dfd
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:30:48 +0530

    feat: implement Forms component

---
+++ client/src/components/Forms/Forms.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Forms.scss';
+
+
+export function Forms() {
+  return (
+    <div className="forms-container">
+      <div id='middle_container'>
+        <h1>Forms</h1>
+      </div>
+    </div>
+  )
+}
