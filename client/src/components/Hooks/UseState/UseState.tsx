20fd3c9c086b2250af37ad54c6cba8b76ac9e5e1
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:34:25 +0530

    feat: implement UseState component

---
+++ client/src/components/Hooks/UseState/UseState.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './UseState.scss';
+
+
+export function UseState() {
+  return (
+    <div className="usestate-container">
+      <div id='middle_container'>
+        <h1>useState</h1>
+      </div>
+    </div>
+  )
+}
