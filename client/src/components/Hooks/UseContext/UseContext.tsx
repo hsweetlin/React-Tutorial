6e13756af437ff52f1f8b854500d4e6f35f5c860
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:31:22 +0530

    feat: implement UseContext component

---
+++ client/src/components/Hooks/UseContext/UseContext.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './UseContext.scss';
+
+
+export function UseContext() {
+  return (
+    <div className="usecontext-container">
+      <div id='middle_container'>
+        <h1>useContext</h1>
+      </div>
+    </div>
+  )
+}
