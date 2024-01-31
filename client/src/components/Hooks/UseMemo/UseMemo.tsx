bcad383ac7bf4773515b97271e2df8f7a79d3cdd
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:32:46 +0530

    feat: implement UseMemo component

---
+++ client/src/components/Hooks/UseMemo/UseMemo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './UseMemo.scss';
+
+
+export function UseMemo() {
+  return (
+    <div className="usememo-container">
+      <div id='middle_container'>
+        <h1>useMemo</h1>
+      </div>
+    </div>
+  )
+}
