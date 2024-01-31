448ef069ee86a321c9e7976b10795179c3ada946
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:33:55 +0530

    feat: implement UseRef component

---
+++ client/src/components/Hooks/UseRef/UseRef.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './UseRef.scss';
+
+
+export function UseRef() {
+  return (
+    <div className="useref-container">
+      <div id='middle_container'>
+        <h1>useRef</h1>
+      </div>
+    </div>
+  )
+}
