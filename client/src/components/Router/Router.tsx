8340e68170c8fdf5f47ed1ceb9e9eb32f7b14112
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:35:23 +0530

    feat: implement Router component

---
+++ client/src/components/Router/Router.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Router.scss';
+
+
+export function RouterFC() {
+  return (
+    <div className="router-container">
+      <div id='middle_container'>
+        <h1>Router</h1>
+      </div>
+    </div>
+  )
+}
