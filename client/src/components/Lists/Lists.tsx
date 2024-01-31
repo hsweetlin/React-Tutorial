894e52e8fb45048f3860f4bc35f7232dfcea3da0
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:35:00 +0530

    feat: implement Lists component

---
+++ client/src/components/Lists/Lists.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Lists.scss';
+
+
+export function Lists() {
+  return (
+    <div className="lists-container">
+      <div id='middle_container'>
+        <h1>Lists</h1>
+      </div>
+    </div>
+  )
+}
