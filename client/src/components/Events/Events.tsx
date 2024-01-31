7ca9c5d0cbb56c8051e7e69ae60fc5e8685288f8
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:30:12 +0530

    feat: implement Events component

---
+++ client/src/components/Events/Events.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Events.scss';
+
+
+export function Events() {
+  return (
+    <div className="events-container">
+      <div id='middle_container'>
+        <h1>Events</h1>
+      </div>
+    </div>
+  )
+}
