1be44c162c07ff80e0e344e4e4a99678fc3fbaa1
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-01-31 21:31:46 +0530

    feat: implement UseEffect component

---
+++ client/src/components/Hooks/UseEffect/UseEffect.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './UseEffect.scss';
+
+
+export function UseEffect() {
+  return (
+    <div className="useeffect-container">
+      <div id='middle_container'>
+        <h1>useEffect</h1>
+      </div>
+    </div>
+  )
+}
