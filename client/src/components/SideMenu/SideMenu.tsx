990077fd1b335a10dc9410eb883af15e1c520eb7
hsweetlin <sweetlinjuliet112001@gmail.com>
2024-02-02 20:31:23 +0530

    chore: add miscellaneous list

--- client/src/components/SideMenu/SideMenu.tsx
+++ client/src/components/SideMenu/SideMenu.tsx
@@ -17,30 +17,32 @@ export const SideMenu: React.FC = () => {
         > <Link to={"/react-jsx"} className="menu-link">React JSX</Link></li>
         <li className={location.pathname === '/components' ? 'selected' : ''}>
           <Link to={"/components"} className="menu-link">Components</Link></li>
-        <li className={location.pathname === '/events' ? 'selected' : ''}> 
+        <li className={location.pathname === '/events' ? 'selected' : ''}>
           <Link to={"/events"} className="menu-link">Events</Link></li>
-        <li  className={location.pathname === '/lists' ? 'selected' : ''}> 
+        <li className={location.pathname === '/lists' ? 'selected' : ''}>
           <Link to={"/lists"} className="menu-link">Lists</Link></li>
-        <li  className={location.pathname === '/forms' ? 'selected' : ''}> 
+        <li className={location.pathname === '/forms' ? 'selected' : ''}>
           <Link to={"/forms"} className="menu-link">Forms</Link></li>
         <li className={location.pathname === '/router' ? 'selected' : ''}>
-             <Link to={"/router"} className="menu-link">Router</Link></li>
-        <li  className={location.pathname === '/styling' ? 'selected' : ''}> 
+          <Link to={"/router"} className="menu-link">Router</Link></li>
+        <li className={location.pathname === '/styling' ? 'selected' : ''}>
           <Link to={"/styling"} className="menu-link">React Styling</Link></li>
         <h1>React Hooks</h1>
-        <li  className={location.pathname === '/use_state' ? 'selected' : ''}> 
+        <li className={location.pathname === '/use_state' ? 'selected' : ''}>
           <Link to={"/use_state"} className="menu-link">useState</Link></li>
-        <li  className={location.pathname === '/use_effect' ? 'selected' : ''}>
-             <Link to={"/use_effect"} className="menu-link">useEffect</Link></li>
-        <li  className={location.pathname === '/use_ref' ? 'selected' : ''}> 
+        <li className={location.pathname === '/use_effect' ? 'selected' : ''}>
+          <Link to={"/use_effect"} className="menu-link">useEffect</Link></li>
+        <li className={location.pathname === '/use_ref' ? 'selected' : ''}>
           <Link to={"/use_ref"} className="menu-link">useRef</Link></li>
-        <li  className={location.pathname === '/use_memo' ? 'selected' : ''}> 
+        <li className={location.pathname === '/use_memo' ? 'selected' : ''}>
           <Link to={"/use_memo"} className="menu-link">useMemo</Link></li>
-        <li  className={location.pathname === '/use_reducer' ? 'selected' : ''}> 
+        <li className={location.pathname === '/use_reducer' ? 'selected' : ''}>
           <Link to={"/use_reducer"} className="menu-link">useReducer</Link></li>
-        <li  className={location.pathname === '/use_context' ? 'selected' : ''}>
-             <Link to={"/use_context"} className="menu-link">useContext</Link></li>
-
+        <li className={location.pathname === '/use_context' ? 'selected' : ''}>
+          <Link to={"/use_context"} className="menu-link">useContext</Link></li>
+        <h1>Miscellaneous</h1>
+        <li className={location.pathname === '/react_icons' ? 'selected' : ''}>
+          <Link to={"/react_icons"} className="menu-link">React-icons</Link></li>
       </ul>
     </nav>
   )
